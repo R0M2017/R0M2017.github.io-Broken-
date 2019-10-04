391ed57ccccc8e5e7acbbd6e6fd23e067804515c
Roman <30502931+R0M2017@users.noreply.github.com>
2019-10-03 19:24:11 -0700

    color switch
    Should be able to change body color based on toggle

--- js/tswitch.js
+++ js/tswitch.js
@@ -1,8 +1,9 @@
 function switchdarkmode() {
     var darkmode = document.getElementById("switchToggle");
-
+    var bodycolor = document.getElementsByClassName("mainbody").style.color;
     if (darkmode.checked == true) {
         console.log("darkmode");
+        bodycolor = "#333333";
     } else {
         console.log("normal");
     }
