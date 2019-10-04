a0e4662781e854404d27bdd353b2705c803965e9
Roman <30502931+R0M2017@users.noreply.github.com>
2019-10-03 19:27:01 -0700

    class to id
    added an id value to the class mainbody.

--- js/tswitch.js
+++ js/tswitch.js
@@ -1,9 +1,9 @@
 function switchdarkmode() {
     var darkmode = document.getElementById("switchToggle");
-    var bodycolor = document.getElementsByClassName("mainbody").style.color;
+    var bodycolor = document.getElementById("mainbody");
     if (darkmode.checked == true) {
         console.log("darkmode");
-        bodycolor = "#333333";
+        bodycolor.style.color = "#333333";
     } else {
         console.log("normal");
     }
