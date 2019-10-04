973a86fa84efbef7551d0cf73eabb62ac70cc57f
Roman <30502931+R0M2017@users.noreply.github.com>
2019-10-03 20:18:33 -0700

    more darkmode
    added darkmode to the containerbody and also the fonts

--- js/tswitch.js
+++ js/tswitch.js
@@ -2,13 +2,18 @@ function switchdarkmode() {
     var darkmode = document.getElementById("switchToggle");
     var body = document.getElementById("mainbody");
     var nav = document.getElementById("mainnav");
+    var containerbody = document.getElementById("containerbody");
     if (darkmode.checked == true) {
         console.log("darkmode");
         body.style.backgroundColor = "#333333";
         nav.style.backgroundColor = "#1a0d00";
+        containerbody.style.backgroundColor = "#333333";
+        containerbody.style.color = "#f9f9f9";
     } else {
         console.log("normal");
         body.style.backgroundColor = "#f9f9f9";
         nav.style.backgroundColor = "#f9f9f9";
+        containerbody.style.backgroundColor = "#f9f9f9";
+        containerbody.style.color = "#333333"
     }
 }
