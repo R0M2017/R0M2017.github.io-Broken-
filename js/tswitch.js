a6e45717ae9038522450287861815dc1e5f3e29d
Roman <30502931+R0M2017@users.noreply.github.com>
2019-10-03 20:13:36 -0700

    lambda to function
    changed the lambda function back to a regular function because it didn't want to work for some reason

--- js/tswitch.js
+++ js/tswitch.js
@@ -1,4 +1,4 @@
-var switchdarkmode = () => {
+function switchdarkmode() {
     var darkmode = document.getElementById("switchToggle");
     var body = document.getElementById("mainbody");
     var nav = document.getElementById("mainnav");
